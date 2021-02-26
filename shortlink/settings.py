22e51dc93468d742c221367e05a3437a45048e84
RzhepkoV <23-rva@mail.ru>
2021-02-26 23:25:03 +0300

    Add django-heroku

--- shortlink/settings.py
+++ shortlink/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'varzhepko@gmail.com'
 EMAIL_HOST_PASSWORD = 'jihzvsszwgmypoul'
+
+django_heroku.settings(locals())
